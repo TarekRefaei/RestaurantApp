bdeb299ef4cf5db5bbd7cade8f993c76ff38020f
TarekRefaei <refa3igroup@gmail.com>
2022-07-28 02:05:59 +0200

    Restaurant Delivery App with Flutter

--- lib/models/restaurant_model.dart
+++ lib/models/restaurant_model.dart
@@ -2,75 +2,75 @@ import 'package:equatable/equatable.dart';
 import 'package:flutter/material.dart';
 import 'models.dart';
 
-class Restaurent extends Equatable {
+class Restaurant extends Equatable {
   final int id;
   final String name;
-  final Image restaurentImage;
+  final Image restaurantImage;
   final List<String> tags;
   final List<MenuItem> menuItems;
   final int deliveryTime;
   final double deliveryFee;
-  final double destance;
+  final double distance;
 
-  Restaurent(
+  Restaurant(
       {required this.id,
       required this.name,
       required this.menuItems,
-      required this.restaurentImage,
+      required this.restaurantImage,
       required this.tags,
       required this.deliveryTime,
       required this.deliveryFee,
-      required this.destance});
+      required this.distance});
 
   @override
   List<Object?> get props =>
-      [id, name, restaurentImage, tags, deliveryFee, deliveryTime, destance];
+      [id, name, restaurantImage, tags, deliveryFee, deliveryTime, distance];
 
-  static List<Restaurent> restaurants = [
-    Restaurent(
+  static List<Restaurant> restaurants = [
+    Restaurant(
       id: 1,
       name: "PAPAJones",
-      restaurentImage: Image.asset("assets/logo/papajohnes.png"),
+      restaurantImage: Image.asset("assets/logo/papajohnes.png"),
       tags: const ["Pizza", "Pasta"],
       deliveryTime: 10,
       deliveryFee: 0.2,
-      destance: 30,
+      distance: 30,
       menuItems: MenuItem.menuItems
           .where((menuitem) => menuitem.restaurantId == 1)
           .toList(),
     ),
-    Restaurent(
+    Restaurant(
       id: 2,
       name: "jhony Rocket",
-      restaurentImage: Image.asset("assets/logo/jhony rocket.png"),
+      restaurantImage: Image.asset("assets/logo/jhony rocket.png"),
       tags: const ["burger", "FastFood"],
       deliveryTime: 15,
       deliveryFee: 0.5,
-      destance: 10,
+      distance: 10,
       menuItems: MenuItem.menuItems
           .where((menuitem) => menuitem.restaurantId == 2)
           .toList(),
     ),
-    Restaurent(
+    Restaurant(
       id: 3,
       name: "StarBucks",
-      restaurentImage: Image.asset("assets/logo/starbucks.png"),
+      restaurantImage: Image.asset("assets/logo/starbucks.png"),
       tags: const ["cola", "Drinks"],
       deliveryTime: 50,
       deliveryFee: 1.5,
-      destance: 19,
+      distance: 19,
       menuItems: MenuItem.menuItems
           .where((menuitem) => menuitem.restaurantId == 3)
           .toList(),
     ),
-    Restaurent(
+    Restaurant(
       id: 4,
       name: "Denny",
-      restaurentImage: Image.asset("assets/logo/denny.png"),
+      restaurantImage: Image.asset("assets/logo/denny.png"),
       tags: const ["Pizza", "Drinks", "Fast Food", "Ice Cream", "Deserts"],
       deliveryTime: 30,
       deliveryFee: 0.4,
-      destance: 14,
+      distance: 14,
       menuItems: MenuItem.menuItems
           .where((menuItem) => menuItem.restaurantId == 4)
           .toList(),
